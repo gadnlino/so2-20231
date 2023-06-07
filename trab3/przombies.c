82ecc11e224efac8715f4d53e686333f4ed6b13a
Guilherme Avelino <guiavenas@gmail.com>
2023-06-07 00:41:01 -0300

    gerando arquivo de logs

--- trab3/przombies.c
+++ trab3/przombies.c
@@ -4,22 +4,75 @@
 #include <unistd.h>
 #include <string.h>
 
-void finish(){
-    //TODO: terminate gracefully
+const char *ARQUIVO_LOG = "./saida.log";
+
+void finish()
+{
+    // TODO: terminate gracefully
     exit(0);
 }
 
-void do_nothing(){}
+void do_nothing() {}
 
-int main(int argc, char **argv){
-    
-    int n = atoi(argv[1]);
-    
-    signal(SIGINT, do_nothing);
+void escreve_delimitador()
+{
+    FILE *file;
+
+    file = fopen(ARQUIVO_LOG, "r");
+    if (file == NULL)
+    {
+        printf("File does not exist. Creating and writing to the file.\n");
+        file = fopen(ARQUIVO_LOG, "w");
+
+        fprintf(file, "       PID       PPID Nome do Programa\n");
+        fprintf(file, "=========================================\n");
+        fclose(file);
+    }
+    else
+    {
+        fclose(file);
+        file = fopen(ARQUIVO_LOG, "a");
+        fprintf(file, "=========================================\n");
+        fclose(file);
+    }
+}
+
+void write_zombie_processes()
+{
+    char command[97] = "ps -e -o s,pid,ppid,comm | grep '^Z' | cut -d' ' -f3- | rev | cut -d' ' -f2- | rev >> ";
+    strcat(command, ARQUIVO_LOG);
+    system(command);
+}
+
+int main(int argc, char **argv)
+{
+
+    if (fork())
+    {
+        exit(0);
+    }
+
+    int i;
+
+    for (i = 1; i <= 31; i++)
+    {
+        signal(i, do_nothing);
+    }
+
+    signal(SIGINT, finish);
     signal(SIGTERM, finish);
 
-    while(1){
-        printf("I am about to sleep...\n");
+    if (argc < 2) {
+        printf("Passa o parametro, po\n");
+        exit(1);
+    }
+
+    int n = atoi(argv[1]);
+
+    while (1)
+    {
+        escreve_delimitador();
+        write_zombie_processes();
         sleep((unsigned int)n);
     }
 
