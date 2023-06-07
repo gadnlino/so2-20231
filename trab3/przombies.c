2d031f1935cefa4abab7d9b42cbe15e233a6994c
Guilherme Avelino <guiavenas@gmail.com>
2023-06-06 23:00:42 -0300

    sleep parametrizado

--- trab3/przombies.c
+++ trab3/przombies.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <string.h>
 
 void finish(){
     //TODO: terminate gracefully
@@ -11,13 +12,15 @@ void finish(){
 void do_nothing(){}
 
 int main(int argc, char **argv){
-
+    
+    int n = atoi(argv[1]);
+    
     signal(SIGINT, do_nothing);
     signal(SIGTERM, finish);
 
     while(1){
         printf("I am about to sleep...\n");
-        sleep(1);
+        sleep((unsigned int)n);
     }
 
     return 0;
